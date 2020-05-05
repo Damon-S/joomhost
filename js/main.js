552994f3da0cac1c25148f080c3a799c02eca17b
Damon <rasseyaniy85@gmail.com>
2020-05-05 18:36:18 +0300

    joomhost

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@ $(function () {
         arrows: false,
         dots: true,
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        responsive: [{
+            breakpoint: 1106,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }]
+    });
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();
     });
 });
