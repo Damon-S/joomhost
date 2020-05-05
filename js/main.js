5bdd168b43a59aa5868d2d207a902b68d23ec81b
Damon <rasseyaniy85@gmail.com>
2020-05-05 12:41:50 +0300

    joomhost

--- js/main.js
+++ js/main.js
@@ -1,23 +1,8 @@
 $(function () {
     $('.reviews-slider').slick({
-        //     nextArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow"></button>',
-        //     prevArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="prev next"></button>',
         arrows: false,
-        // fade: true,
         dots: true,
         slidesToShow: 2,
         slidesToScroll: 2
-        //     // responsive: [{
-        //     //     breakpoint: 480,
-        //     //     settings: {
-        //     //         arrows: false
-        //     //     }
-        //     // }]
-        // });
-        // $('.header-btn').on('click', function () {
-        //     $('.menu').addClass('active');
-        // });
-        // $('.close-btn').on('click', function () {
-        //     $('.menu').removeClass('active');
     });
 });
